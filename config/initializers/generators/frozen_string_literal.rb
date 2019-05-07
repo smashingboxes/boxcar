ec7e7093b72c23cd026b8dbd4d2cd9a8d9d4c0f6
Derek Kniffin <derek.kniffin@gmail.com>
2019-05-07 14:04:09 -0400

    Add generator extension to add frozen_string_literal comment

---
+++ config/initializers/generators/frozen_string_literal.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+require "rails/generators/migration"
+
+# https://gist.github.com/ta1kt0me/6a7058d16621785d4f7038bde6cd3b98
+
+module AddFrozenStringLiteralComment
+  def add_frozen_string_literal_comment(dist)
+    return unless File.exist?(dist) && File.extname(dist) == ".rb"
+
+    File.open(dist, "r") do |f|
+      body = f.read
+
+      File.open(dist, "w") do |new_f|
+        new_f.write("# frozen_string_literal: true\n\n" + body)
+      end
+    end
+  end
+end
+
+module GeneratorPrepend
+  include AddFrozenStringLiteralComment
+
+  def invoke!
+    res = super
+    add_frozen_string_literal_comment(existing_migration)
+    res
+  end
+end
+
+module TemplatePrepend
+  include AddFrozenStringLiteralComment
+
+  def template(source, *args, &block)
+    res = super
+    add_frozen_string_literal_comment(args.first)
+    res
+  end
+end
+
+Rails::Generators::Actions::CreateMigration.send :prepend, GeneratorPrepend
+Rails::Generators::NamedBase.send :prepend, TemplatePrepend
