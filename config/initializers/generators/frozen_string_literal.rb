a4802bc8f0690163af5382ca688c87ff46168684
Derek Kniffin <derek.kniffin@gmail.com>
2019-05-07 14:15:40 -0400

    Only load frozen_string_literal generator if generators are defined

--- config/initializers/generators/frozen_string_literal.rb
+++ config/initializers/generators/frozen_string_literal.rb
@@ -1,42 +1,44 @@
 # frozen_string_literal: true
 
-require "rails/generators/migration"
-
 # https://gist.github.com/ta1kt0me/6a7058d16621785d4f7038bde6cd3b98
 
-module AddFrozenStringLiteralComment
-  def add_frozen_string_literal_comment(dist)
-    return unless File.exist?(dist) && File.extname(dist) == ".rb"
+if defined?(::Rails::Generators)
+  require "rails/generators/migration"
 
-    File.open(dist, "r") do |f|
-      body = f.read
+  module AddFrozenStringLiteralComment
+    def add_frozen_string_literal_comment(dist)
+      return unless File.exist?(dist) && File.extname(dist) == ".rb"
 
-      File.open(dist, "w") do |new_f|
-        new_f.write("# frozen_string_literal: true\n\n" + body)
+      File.open(dist, "r") do |f|
+        body = f.read
+
+        File.open(dist, "w") do |new_f|
+          new_f.write("# frozen_string_literal: true\n\n" + body)
+        end
       end
     end
   end
-end
 
-module GeneratorPrepend
-  include AddFrozenStringLiteralComment
+  module GeneratorPrepend
+    include AddFrozenStringLiteralComment
 
-  def invoke!
-    res = super
-    add_frozen_string_literal_comment(existing_migration)
-    res
+    def invoke!
+      res = super
+      add_frozen_string_literal_comment(existing_migration)
+      res
+    end
   end
-end
 
-module TemplatePrepend
-  include AddFrozenStringLiteralComment
+  module TemplatePrepend
+    include AddFrozenStringLiteralComment
 
-  def template(source, *args, &block)
-    res = super
-    add_frozen_string_literal_comment(args.first)
-    res
+    def template(source, *args, &block)
+      res = super
+      add_frozen_string_literal_comment(args.first)
+      res
+    end
   end
-end
 
-Rails::Generators::Actions::CreateMigration.send :prepend, GeneratorPrepend
-Rails::Generators::NamedBase.send :prepend, TemplatePrepend
+  Rails::Generators::Actions::CreateMigration.send :prepend, GeneratorPrepend
+  Rails::Generators::NamedBase.send :prepend, TemplatePrepend
+end
