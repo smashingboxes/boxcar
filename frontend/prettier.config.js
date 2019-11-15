0f62e2ec588c97bfcf949dac8098333ca1f6e479
Chris Baker <chrbake2@cisco.com>
2019-11-15 17:32:45 -0500

    fixed inline testing. added in recommended extensions.

--- frontend/prettier.config.js
+++ frontend/prettier.config.js
@@ -1,5 +1,5 @@
 // Some settings automatically inherited from .editorconfig
-
+// taken from https://github.com/iamturns/create-exposed-app which is linked from the airbnb linting rules
 module.exports = {
   // Why include an unnecessary character at the end of every line? Break the habit (automatically)!
   semi: false,
