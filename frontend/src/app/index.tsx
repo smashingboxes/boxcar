97957b7823e3379c4ff662dd6977f6256c91c605
Chris Baker <chrbake2@cisco.com>
2019-11-21 15:52:41 -0500

    this should fail.

--- frontend/src/app/index.tsx
+++ frontend/src/app/index.tsx
@@ -4,7 +4,7 @@ import './App.scss';
 
 const App = () => {
   return (
-    <div onClick={} className="App">
+    <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
